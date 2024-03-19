8f5997875eb870da63756530ccd60674a708cf82
Carlos Figueroa <figcarlos276@gmail.com>
2024-03-18 22:25:11 -0600

    Pruebas youtube

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Crear Cursos')
+
+@section('content')
+    <h1>Bienvenido a la pagina crear</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+Route::controller(CursoController::class)->group(function(){
+    Route::get('cursos', 'index');
+
+    Route::get('cursos/create', 'create');
+
+    Route::get('cursos/{curso}', 'show');
 });
+
+// Route::get('cursos', [CursoController::class, 'index']);
+
+// Route::get('cursos/create', [CursoController::class, 'create']);
+
+// Route::get('cursos/{curso}', [CursoController::class, 'show']);
